712b0953aa753add15ea753d9f8c9893384ef502
Tyler Batts <tyler_batts@yahoo.com>
2019-01-26 18:45:34 -0500

    implemented teleportation ability

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
     [SerializeField] Transform homeSpawn;
     [SerializeField] GameObject player;
-    void Start() {
 
+    bool shouldGoHome = false;
+
+    private void Start() {
+        // SceneManager.LoadScene("Home");
+    }
+
+    private void Update() {
+        if (shouldGoHome) {
+            SceneManager.LoadScene("Home");
+            shouldGoHome = false;
+        }
+    }
+
+    public void GoHome() {
+        shouldGoHome = true;
     }
 }
