8ae01c71b4f0ec72b7608afb785fd83ad96c91d1
Anthony DelSordo <anthonydelsordo@yahoo.com>
2019-01-27 14:55:22 -0500

    Added the ability to equip tools

---
+++ Assets/Scripts/EquippedToolScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedToolScript : MonoBehaviour
+{
+	public static EquippedToolScript instance;
+
+    void Start()
+    {
+        instance = this;
+    }
+}
